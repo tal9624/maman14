b607595615c5e7f161a8f642224dc95a65341ecb
tal9624 <123299775+tal9624@users.noreply.github.com>
2023-01-24 05:36:42 +0200

    Add files via upload

--- mcr2.c
+++ mcr2.c
@@ -3,20 +3,20 @@
 #include <ctype.h>
 #include <string.h>
 
+#define MAXSIZE 1000
 #define LINELEN 81
-#define VARLEN 30 
 /* prototypes */
 void printing(char* buffer,FILE *fp, FILE *fpNew ,char** macNames , char** macConts, int* maci);
-void saveCon(char* buffer , char** macConts,int maci,int i,int *mcrBoolPtr);
-void saveName(char* buffer, char** macNames , char** macConts,int* maci, int i, int* mcrBool); 
+void saveCon(char* buffer , char*** macConts,int *maci,int i,int *mcrBoolPtr);
+void saveName(char* buffer, char*** macNames , char*** macConts,int* maci, int i, int* mcrBool); 
 int main( int argc, char *argv[])
 {	
 	int numberOfArguments = argc;
 	int i = 0;
 	int mcrBool = 0;
 	int maci = -1; /* macro  index */
-	char **macConts = malloc(0 * sizeof(char*)); /*dynamicly initializing size of string array */
-	char **macNames = malloc(0 * sizeof(char*)); /*dynamicly initializing size of string array */
+	char **macConts = NULL; /*dynamicly initializing size of string array */
+	char **macNames = NULL;/*dynamicly initializing size of string array */
 	char buffer[LINELEN];
 	char *kelet = argv[1];
 	FILE *fpNew = NULL;
@@ -50,12 +50,12 @@ int main( int argc, char *argv[])
 		if (buffer[i] == 'm' && buffer[i+1] == 'c' && buffer[i+2] == 'r' && mcrBool == 0) 
 		{
 			/*saving the name of it*/
-			saveName(buffer,  macNames ,  macConts,&maci,i,&mcrBool);	
+			saveName(buffer,  &macNames ,  &macConts,&maci,i,&mcrBool);	
 		}
 		/*reading a macro*/
 		else if ( mcrBool == 1) 
 		{
-			saveCon(buffer,macConts,maci,i,&mcrBool);	 
+			saveCon(buffer,&macConts,&maci,i,&mcrBool);	 
 		}
 		/*not reading a macro and is not a new macro definition line */
 		else if (!(buffer[i] == 'm' && buffer[i+1] == 'c' && buffer[i+2] == 'r') && mcrBool == 0)
@@ -80,38 +80,42 @@ int main( int argc, char *argv[])
 /* this function save the names of the macros in a dynamic array
 for each name well reallocate another space for it
 well expend also the contents array for each name well define */
-void saveName(char* buffer, char** macNames , char** macConts,int* maci,int i , int* mcrBool) 
+void saveName(char* buffer, char*** macNames , char*** macConts,int* maci,int i , int* mcrBool) 
 {
+	
 	int j;
+
 	*mcrBool = 1;
 	(*maci)++;
-	macNames = realloc(macNames,((*maci)+1)*sizeof(char*)); /* extending the array 																	for each macro */
-	macConts = realloc(macConts,((*maci)+1)*sizeof(char*));
-	macConts[*maci] =(char*)malloc(0*sizeof(char));
-	macNames[*maci] =(char*)malloc(VARLEN*sizeof(char));
+	/* extending the array for each macro */
+	*macNames = realloc(*macNames,((*maci)+1)*sizeof(char*));
+	*macConts = realloc(*macConts,((*maci)+1)*sizeof(char*));
+	(*macConts)[*maci] =(char*)malloc((strlen(buffer)+1)*sizeof(char*));
+	(*macNames)[*maci] =(char*)malloc((strlen(buffer)+1)*sizeof(char));
+
 	i+=3; /* skipping the word mcr */
 			
 	for (; isspace(buffer[i]) ; i++); /* arrived to the macro name */
 	for (j = 0; !isspace(buffer[i]) ;j++, i++) /* saving macro name */
-	macNames[(*maci)][j] = buffer[i]; /* to my array of strings */ 
+	(*macNames)[(*maci)][j] = buffer[i]; /* to my array of strings */ 
 }
 /* this function save the contents of the macros in a dynamic array
 for each content line we'll reallocate another space for it,
 as the size of the line*/
-void saveCon(char* buffer , char** macConts,int maci,int i,int* mcrBool)
+void saveCon(char* buffer , char*** macConts,int *maci,int i,int* mcrBool)
 {
-	if (!(buffer[i] == 'e' && buffer[i+1] == 'n' && buffer[i+2] == 'd' /* not... */
+	
+	if (!(buffer[i] == 'e' && buffer[i+1] == 'n' && buffer[i+2] == 'd' /* not... */ /* strncmp6*/
 	&& buffer[i+3] == 'm' && buffer[i+4] == 'c' &&  buffer[i+5] == 'r')) /* ...endmcr*/
 		{/* inserting the content of the macro to maccontent */
-		macConts[maci] = realloc(macConts[maci],LINELEN*sizeof(char));
-		strcat(macConts[maci],buffer);
-		macConts[maci+1] = '\0';	
+		 (*macConts)[*maci] = realloc((*macConts)[*maci],(strlen((*macConts)[*maci]+1)+(strlen(buffer)+1))*sizeof(char*));
+		strcat((*macConts)[*maci],buffer);
+	 
 		}
-	else if (buffer[i] == 'e' && buffer[i+1] == 'n' && buffer[i+2] == 'd'
-	&& buffer[i+3] == 'm' && buffer[i+4] == 'c' &&  buffer[i+5] == 'r')/* endmcr */
-			{
-			*mcrBool = 0;
-			}
+	else /* endmcr */
+		{
+		*mcrBool = 0;
+		}
 }
 /*printing to a temp file the requested */				
 void printing(char* buffer,FILE *fp, FILE *fpNew ,char** macNames , char** macConts, int *maci)	
@@ -126,8 +130,10 @@ void printing(char* buffer,FILE *fp, FILE *fpNew ,char** macNames , char** macCo
 		/*checking if the first word in the line is equal
 		to one of the macro names*/
 		/* if equal*/
+
 		if((strcmp(firstRead,macNames[i]) == 0))
 		{
+		
 		equal = 1;
 		fputs(macConts[i],fpNew); 
 		}	
